f5cfa91173b1758315addf55eb0c6f196e0b4c71
Aniket Maurya <theaniketmaurya@gmail.com>
2025-10-16 11:39:00 +0100

    fixes

--- src/superauth/config.py
+++ src/superauth/config.py
@@ -7,8 +7,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     """Centralized configuration class for all environment variables using Pydantic Settings."""
 
-    apollo_api_key: Optional[SecretStr] = Field("", env="APOLLO_API_KEY")
-    celesto_api_key: Optional[SecretStr] = Field("", env="CELESTO_API_KEY")
+    apollo_api_key: Optional[SecretStr] = Field(default="", alias="APOLLO_API_KEY")
+    celesto_api_key: Optional[SecretStr] = Field(default="", alias="CELESTO_API_KEY")
 
 
     model_config = SettingsConfigDict(case_sensitive=False, extra="ignore", env_file=".env", env_file_encoding="utf-8")
